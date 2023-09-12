325fb39cadb9ee381ab79c06ae02c27e0c185bc0
jekwupeter <cephaspeter123@gmail.com>
2023-09-12 22:48:00 +0100

    changed route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request
 # import flask
 app = Flask(__name__)
 
-@app.route("/getdata", methods=["GET"])
+@app.route("/api", methods=["GET"])
 def get_user_data():
     slack_name = request.args.get("slack_name")
     track = request.args.get("track")
